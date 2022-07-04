4f66deaaf27c2553c87fe070c947ce3ff36b16c1
Marcus Vinicius Estevam Candido <marcus.v.e.candido@gmail.com>
2022-07-04 16:55:20 -0300

    Challenge finished

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,64 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Champion;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		/*Entendi a referência */
 
+		System.out.println("Digite os dados do primeiro campeão:");
+		System.out.print("Nome: ");
+		String nameA = sc.nextLine();
+		System.out.print("Vida inicial: ");
+		int lifeA = sc.nextInt();
+		System.out.print("Ataque: ");
+		int attackA = sc.nextInt();
+		System.out.print("Armadura: ");
+		int armorA = sc.nextInt();
+
+		System.out.println();
+		System.out.println("Digite os dados do segundo campeão:");
+		System.out.print("Nome: ");
+		sc.nextLine();
+		String nameB = sc.nextLine();
+		System.out.print("Vida inicial: ");
+		int lifeB = sc.nextInt();
+		System.out.print("Ataque: ");
+		int attackB = sc.nextInt();
+		System.out.print("Armadura: ");
+		int armorB = sc.nextInt();
+
+		Champion champA = new Champion(nameA, lifeA, attackA, armorA);
+		Champion champB = new Champion(nameB, lifeB, attackB, armorB);
+
+		System.out.println();
+		System.out.print("Quantos turnos você deseja executar? ");
+		int number = sc.nextInt();
+		int i = 1;
+		do {
+			champA.takeDamege(champB);
+			champB.takeDamege(champA);
+			System.out.println();
+			System.out.println("Resultado do turno " + i + ": ");
+			System.out.println(champA.status());
+			System.out.println(champB.status());
+			if (champA.getLife() == 0 || champB.getLife() == 0) {
+				i = number + 1;
+			} else {
+				i++;
+			}
+		} while (i <= number);
+
+		System.out.println();
+		System.out.println("FIM DO COMBATE");
+
+		sc.close();
 	}
 
 }
--- src/entities/Champion.java
+++ src/entities/Champion.java
@@ -2,4 +2,62 @@ package entities;
 
 public class Champion {
 
+	private String name;
+	private int life;
+	private int attack;
+	private int armor;
+
+	public Champion(String name, int life, int attack, int armor) {
+		this.name = name;
+		this.life = life;
+		this.attack = attack;
+		this.armor = armor;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getAttack() {
+		return attack;
+	}
+
+	public int getLife() {
+		return life;
+	}
+
+	public int getArmor() {
+		return armor;
+	}
+
+	public void setArmor(int armor) {
+		this.armor = armor;
+	}
+
+	public void takeDamege(Champion championB) {
+		if (this.armor >= championB.getAttack()) {
+			this.life -= 1;
+		} else {
+
+			this.life = getLife() - (championB.getAttack() - getArmor());
+		}
+		if (this.life < 0) {
+			this.life = 0;
+		}
+	}
+
+	public String status() {
+		StringBuffer bf = new StringBuffer();
+
+		bf.append(getName() + ": ");
+
+		if (getLife() == 0) {
+			bf.append(+getLife() + " de vida (morreu)");
+		} else {
+			bf.append(+getLife() + " de vida");
+		}
+		return bf.toString();
+
+	}
+
 }
